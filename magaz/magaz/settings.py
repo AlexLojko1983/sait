803d7ecd346af57a289cb409c24516e0b03099ef
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2023-04-18 17:07:06 +0300

    1

--- magaz/magaz/settings.py
+++ magaz/magaz/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-15kow35cbrydso^so((sq(hl!lbh8!9))v)40j-#t99g=l#+_h'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
